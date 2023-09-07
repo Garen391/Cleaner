6a4bc5a673f6551b025b3ec7bc863f8ebded5572
Garen391 <63588765+Garen391@users.noreply.github.com>
2023-09-07 15:25:09 +0300

    Delete MappingEditorGUI directory

--- MappingEditorGUI/src/main/java/notkek/tubik/utils/log/LoggerUtils.java
+++
@@ -1,9 +0,0 @@
-package notkek.tubik.utils.log;
-
-import static notkek.tubik.MappingEditorGUI.logArea;
-
-public class LoggerUtils {
-    public static void log(String message) {
-        logArea.appendText(message + "\n");
-    }
-}
